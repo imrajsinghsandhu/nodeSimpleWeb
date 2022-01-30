3cd82a2eb5df3639afbacb4970475275a75ad0e3
imrajsingh <e0543951@u.nus.edu>
2022-01-31 00:00:28 +0800

    delete some css files

--- src/components/ButtonRow.js
+++ src/components/ButtonRow.js
@@ -7,15 +7,14 @@ const ButtonRow = ({
   handleBeachClick,
   handleFoodClick,
 }) => {
-  console.log("Button Row works");
   return (
     <>
       <Button size="large" onClick={handleBirdClick}>
         Birds
       </Button>
       <Button onClick={handleFoodClick}>Food</Button>
-      <Button onClick={handleMountainClick}> Mountains</Button>
-      <Button onClick={handleBeachClick}> Beaches</Button>
+      <Button onClick={handleMountainClick}>Mountains</Button>
+      <Button onClick={handleBeachClick}>Beaches</Button>
     </>
   );
 };
