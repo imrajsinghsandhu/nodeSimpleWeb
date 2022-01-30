9d545328027ed48ca9f4f80e112752d3e8b3f5df
imrajsingh <e0543951@u.nus.edu>
2022-01-31 01:35:33 +0800

    add some css styling

--- src/components/ButtonRow.js
+++ src/components/ButtonRow.js
@@ -1,5 +1,6 @@
 import { Button, Row } from "antd";
 import React from "react";
+import "./ButtonRow.css";
 
 const ButtonRow = ({
   handleBirdClick,
@@ -7,15 +8,43 @@ const ButtonRow = ({
   handleBeachClick,
   handleFoodClick,
 }) => {
+  const buttonData = [
+    {
+      id: 1,
+      name: "Birds",
+      function: handleBirdClick,
+    },
+    {
+      id: 2,
+      name: "Food",
+      function: handleFoodClick,
+    },
+    {
+      id: 3,
+      name: "Mountains",
+      function: handleMountainClick,
+    },
+    {
+      id: 4,
+      name: "Beaches",
+      function: handleBeachClick,
+    },
+  ];
+
+  const renderRow = (item) => {
+    return (
+      <div key={item.id}>
+        <Button onClick={item.function} className="button">
+          {item.name}
+        </Button>
+      </div>
+    );
+  };
+
   return (
-    <>
-      <Button size="large" onClick={handleBirdClick}>
-        Birds
-      </Button>
-      <Button onClick={handleFoodClick}>Food</Button>
-      <Button onClick={handleMountainClick}>Mountains</Button>
-      <Button onClick={handleBeachClick}>Beaches</Button>
-    </>
+    <div className="button_row">
+      {buttonData.map((item) => renderRow(item))}
+    </div>
   );
 };
 
--- src/components/PicItem.js
+++ src/components/PicItem.js
@@ -1,23 +1,44 @@
 import { Row } from "antd";
 import React from "react";
+import "./PicItem.css";
 
 const PicItem = ({ picData }) => {
   return (
     <Row>
-      <img src={picData.first} height={200} width={200} />
-      <img src={picData.second} height={200} width={200} />
-      <img src={picData.third} height={200} width={200} />
-      <img src={picData.fourth} height={200} width={200} />
+      <img className="pic_item" src={picData.first} height={200} width={200} />
+      <img className="pic_item" src={picData.second} height={200} width={200} />
+      <img className="pic_item" src={picData.third} height={200} width={200} />
+      <img className="pic_item" src={picData.fourth} height={200} width={200} />
       <Row />
       <Row
         style={{
           padding: 15,
         }}
       >
-        <img src={picData.fifth} height={200} width={200} />
-        <img src={picData.sixth} height={200} width={200} />
-        <img src={picData.seventh} height={200} width={200} />
-        <img src={picData.eighth} height={200} width={200} />
+        <img
+          className="pic_item"
+          src={picData.fifth}
+          height={200}
+          width={200}
+        />
+        <img
+          className="pic_item"
+          src={picData.sixth}
+          height={200}
+          width={200}
+        />
+        <img
+          className="pic_item"
+          src={picData.seventh}
+          height={200}
+          width={200}
+        />
+        <img
+          className="pic_item"
+          src={picData.eighth}
+          height={200}
+          width={200}
+        />
       </Row>
     </Row>
   );
